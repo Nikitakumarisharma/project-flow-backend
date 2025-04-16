94d2180058132e367d7a0f3b15ad7f415a4678c9
Nikitakumarisharma <113896209+Nikitakumarisharma@users.noreply.github.com>
2025-04-16 18:04:42 +0530

    refactor: clean up code formatting and improve readability in index.js

--- api/index.js
+++ api/index.js
@@ -1,35 +1,35 @@
-const express = require("express");
-const serverless = require("serverless-http"); // ✅ required for Vercel
-const cors = require("cors");
-const mongoose = require("mongoose");
+const express= require('express');
+const serverless = require("serverless-http"); // ✅ Important for Vercel
+const app= express();
+const cors= require('cors');
 
-const app = express();
-
-app.use(cors({ origin: "*" }));
+app.use(cors({origin: '*'}));
 app.use(express.json()); // ✅ for parsing JSON payloads
 
-// ✅ MongoDB connect
-mongoose
-  .connect(
-    "mongodb+srv://niku2003kumari:niku2003@cmtai-crm-cluster.akuljso.mongodb.net/?retryWrites=true&w=majority",
+const mongoose= require('mongoose');
+mongoose.connect(
+    'mongodb+srv://niku2003kumari:niku2003@cmtai-crm-cluster.akuljso.mongodb.net/?retryWrites=true&',
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     }
   )
-  .then(() => console.log("✅ MongoDB Atlas connected!"))
-  .catch((err) => console.error("❌ MongoDB connection error:", err.message));
+  .then(() => console.log('✅ MongoDB Atlas connected!'))
+  .catch((err) => console.error('❌ MongoDB connection error:', err.message));
 
-// ✅ Importing Routes
-const Post_route = require("../routes/postRoute");
-const userRoutes = require("../routes/userRoute");
-const authRoutes = require("../routes/authRoute");
-const projectRoutes = require("../routes/projectRoute");
+const Post_route= require('../routes/postRoute');
+const userRoutes= require('../routes/userRoute');
+const authRoutes= require('../routes/authRoute');
+const projectRoutes= require('../routes/projectRoute');
 
-// ✅ Routes middleware
-app.use("/api", Post_route);
-app.use("/api/users", userRoutes);
-app.use("/api/auth", authRoutes);
-app.use("/api/projects", projectRoutes);
+app.use('/api',Post_route);
+app.use('/api/users',userRoutes);
+app.use('/api/auth',authRoutes);
+app.use('/api/projects',projectRoutes);
+
+
+// app.listen(4000,()=>{
+//     console.log('Server is running on port 4000');
+// })
 
 module.exports = serverless(app);
